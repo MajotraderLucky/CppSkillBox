edfe4b8584ce6aa271e0a22eb828ad2fa13200cb
MajotraderLucky <majohead@gmail.com>
2023-11-18 11:06:59 +0300

    feat(knightmove2): Add checks for incorrect user input

--- Module8th/knightmove2.cpp
+++ Module8th/knightmove2.cpp
@@ -1,13 +1,7 @@
 #include <iostream>
 #include <cmath>
 #include <iomanip>
-
-int convertDoubleToInt(double number) {
-  double fractionalPart = number - std::trunc(number); // отсекаем целую часть
-  double tenthPart = fractionalPart * 10; // берем десятую часть числа
-  int convertedNumber = static_cast<int>(tenthPart);
-  return convertedNumber;
-}
+#include <limits>
 
 double getDoubleFromInput() {
   double input;
@@ -15,39 +9,56 @@ double getDoubleFromInput() {
   while (true) {
     std::cin >> input;
     
-    if (std::cin.fail()) { // если предыдущее извлечение не удалось,
+    // проверяем, что ввод не вызвал ошибку и что число не отрицательно
+    if (std::cin.fail() || input < 0) { 
       std::cin.clear(); // возвращаем cin в 'обычный' режим работы
       std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // удаляем значения предыдущего ввода из входного буфера
-      std::cout << "Пожалуйста, введите корректное число:\n";
-    } 
-    else if (input < 0) {
-      std::cout << "Пожалуйста, введите положительное число:\n";
-    } 
-    else {
-      break;
+      std::cout << "Please enter a positive number:\n";
+    } else {
+      // проверяем, что дробная часть числа находится в промежутке от 0 до 7
+      int converted = static_cast<int>(input * 10) % 10;
+
+      if (converted < 0 || converted > 7) {
+        std::cout << "Please enter a number greater than or equal to 0.0 and less than or equal to 0.7:\n";
+      } else {
+        break;
+      }
     }
   }
   
-  return input;  
+  return input;
+}
+
+int convertDoubleToInt(double number) {
+  double fractionalPart = number - std::trunc(number);
+  int convertedNumber = static_cast<int>(fractionalPart * 10);
+  return convertedNumber;
 }
 
 int main() {
-   // Запрос ввода координат коня
-  std::cout << "Введите местоположение коня:\n";
+   // Request for the input of the knight's coordinates
+  std::cout << "Enter the location of the knight:\n";
   double knightX = getDoubleFromInput();
   double knightY = getDoubleFromInput();
 
-  std::cout << "Введите местоположение точки на доске:\n";
+  std::cout << "Enter a point on the board:\n";
   double pointX = getDoubleFromInput();
   double pointY = getDoubleFromInput();
+
+    std::cout << "-----------------------" << std::endl;
+
+  std::cout << knightX << "--" << knightY << "\n";
+  std::cout << pointX << "--" << pointY << "\n";
 
   int knightXint = convertDoubleToInt(knightX);
   int knightYint = convertDoubleToInt(knightY);
   int pointXint = convertDoubleToInt(pointX);
   int pointYint = convertDoubleToInt(pointY);
 
+  std::cout << "-----------------------" << std::endl;
   std::cout << knightXint << "--" << knightYint << "\n";
   std::cout << pointXint << "--" << pointYint << "\n";
+  std::cout << "-----------------------" << std::endl;
 
   return 0;
 }
