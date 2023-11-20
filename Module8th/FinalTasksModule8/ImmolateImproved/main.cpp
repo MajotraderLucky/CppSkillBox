ac63169db947d79d24a44f1a0d91d965cb50ceed
MajotraderLucky <majohead@gmail.com>
2023-11-21 01:59:15 +0300

    feat(ImmolateImproved): The programm works correctly

--- Module8th/FinalTasksModule8/ImmolateImproved/main.cpp
+++ Module8th/FinalTasksModule8/ImmolateImproved/main.cpp
@@ -1,4 +1,19 @@
 #include <iostream>
+#include <limits>
+#include <string>
+
+bool validateInput(const std::string& input) {
+    try {
+        double value = std::stod(input);
+        if (value < 0 || value > 1) {
+            return false;
+        }
+    } catch (std::exception&) {
+        return false;
+    }
+
+    return true;
+}
 
 int main() {
     double health;
@@ -6,35 +21,53 @@ int main() {
 
     // Ввод данных пользователя
     std::cout << "Введите количество здоровья орка (от 0 до 1): ";
-    std::cin >> health;
+    std::string inputHealth;
+    std::getline(std::cin, inputHealth);
 
     // Проверка на корректность введенного значения здоровья
-    if (health < 0 || health > 1) {
+    if (!validateInput(inputHealth)) {
         std::cout << "Ошибка! Количество здоровья должно быть числом от 0 до 1. Программа завершена.\n";
         return 0;
     }
-    
+
+    health = std::stod(inputHealth);
+
     // Ввод сопротивляемости магии орка
     std::cout << "Введите сопротивляемость магии орка (от 0 до 1): ";
-    std::cin >> resistance;
+    std::string inputResistance;
+    std::getline(std::cin, inputResistance);
 
     // Проверка на корректность введенного значения сопротивляемости магии
-    if (resistance < 0 || resistance > 1) {
+    if (!validateInput(inputResistance)) {
         std::cout << "Ошибка! Сопротивляемость магии должна быть числом от 0 до 1. Программа завершена.\n";
         return 0;
     }
 
-    while (health > 0) {
+    resistance = std::stod(inputResistance);
+
+    bool orcAlive = true;
+
+    while (health > 0 && orcAlive) {
         // Ввод мощности удара
-        double power;
         std::cout << "Введите мощность огненного шара (от 0 до 1): ";
-        std::cin >> power;
+        std::string inputPower;
+        std::getline(std::cin, inputPower);
 
         // Проверка введенной мощности удара
-        if (power < 0 || power > 1) {
-            std::cout << "Ошибка! Мощность должна быть числом от 0 до 1. Попробуйте ещё раз.\n";
+        if (!validateInput(inputPower)) {
+            std::cout << "Ошибка! Мощность должна быть числом от 0 до 1. Попробуйте ещё раз или введите 'стоп' для завершения программы.\n";
+
+            // Проверка на ввод строки "стоп"
+            std::string userInput;
+            std::getline(std::cin, userInput);
+            if (userInput == "стоп") {
+                std::cout << "Программа завершена." << std::endl;
+                return 0;
+            }
             continue;
         }
+
+        double power = std::stod(inputPower);
 
         // Расчет урона с учетом сопротивляемости магии
         double damage = power * (1 - resistance);
@@ -45,6 +78,7 @@ int main() {
         // Проверка на возможность сопротивления магии
         if (health < 0) {
             health = 0;
+            orcAlive = false;
         }
 
         // Вывод результатов
@@ -52,6 +86,11 @@ int main() {
         std::cout << "Оставшиеся очки здоровья орка: " << health << std::endl;
     }
 
-    std::cout << "Орк погиб!" << std::endl;
+    if (orcAlive) {
+        std::cout << "Орк погиб!" << std::endl;
+    } else {
+        std::cout << "Программа завершена." << std::endl;
+    }
+
     return 0;
 }
