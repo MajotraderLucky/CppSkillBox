8cb21bfa5429c82d5a6ea067c1dafc2c48f6cffc
MajotraderLucky <majohead@gmail.com>
2023-11-24 16:13:20 +0300

    feat(ImmolateImproved): Receive all incoming data from the user

--- Module8th/FinalTasksModule8/ImmolateImproved/main.cpp
+++ Module8th/FinalTasksModule8/ImmolateImproved/main.cpp
@@ -7,6 +7,8 @@ int main() {
     std::string userInput;
     double number;
     bool stopCommand;
+    double health, resistance, ballPower;
+
 
     do {
         std::cout << "Введите количество здоровья орка (от 0.0001 до 1): ";
@@ -31,6 +33,66 @@ int main() {
 
     std::cout << " Получено число: " << number << std::endl;
     std::cout << " Получена строка: " << stopCommand << std::endl;
+
+    health = number;
+    std::cout << "Здоровье орка: " << health << std::endl;
+
+    do {
+        std::cout << "Введите сопротивляемость магии орка (от 0.0001 до 1): ";
+        std::getline(std::cin, userInput);
+
+        std::pair<double, bool> result = processUserInput(userInput);
+        printResult(result);
+
+        number = result.first;
+        stopCommand = result.second;
+
+        if (stopCommand == 1) {
+            std::cout << "Программа завершена!" << std::endl;
+            return 0;
+        }
+
+        if (number == 0) {
+            std::cout << "Ошибка! Количество сопротивляемости должно быть числом от 0.0001 до 1. Для завершения программы введите 'stop'" << std::endl;
+        }
+
+    } while (number == 0);
+
+    std::cout << " Получено число: " << number << std::endl;
+    std::cout << " Получена строка: " << stopCommand << std::endl;
+
+    resistance = number;
+    std::cout << "Здоровье орка: " << health << std::endl;
+    std::cout << "Сопротивляемость магии орка: " << resistance << std::endl;
+
+    do {
+        std::cout << "Введите мощность огненного  шара (от 0.0001 до 1): ";
+        std::getline(std::cin, userInput);
+
+        std::pair<double, bool> result = processUserInput(userInput);
+        printResult(result);
+
+        number = result.first;
+        stopCommand = result.second;
+
+        if (stopCommand == 1) {
+            std::cout << "Программа завершена!" << std::endl;
+            return 0;
+        }
+
+        if (number == 0) {
+            std::cout << "Ошибка! Мощность магического шара должна быть числом от 0.0001 до 1. Для завершения программы введите 'stop'" << std::endl;
+        }
+
+    } while (number == 0);
+
+    std::cout << " Получено число: " << number << std::endl;
+    std::cout << " Получена строка: " << stopCommand << std::endl;
+
+    ballPower = number;
+    std::cout << "Здоровье орка: " << health << std::endl;
+    std::cout << "Сопротивляемость магии орка: " << resistance << std::endl;
+    std::cout << "Мощность магического шара: " << ballPower << std::endl;
 
     return 0;
 }
