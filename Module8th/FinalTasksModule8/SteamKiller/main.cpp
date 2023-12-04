6308ee576eb0fab4c3ce83c03f2dfaa2aea9b1be
MajotraderLucky <majohead@gmail.com>
2023-12-04 16:57:54 +0300

    feat(main): Add user`s input

--- Module8th/FinalTasksModule8/SteamKiller/main.cpp
+++ Module8th/FinalTasksModule8/SteamKiller/main.cpp
@@ -1,5 +1,31 @@
 #include <iostream>
+#include <string>
+#include <complex>
+#include "usersinputcontrol.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    double fileSize, speedConnect;
+
+    std::pair<double, bool> userInput;
+
+    userInput = getUserInput(
+            "Укажите размер файла для скачивания в байтах: ",
+            0.001, 1000000000,
+            "Ошибка: вы указали некорректный размер файла. Для завершения программы введите 'stop'\n"
+);
+    if (userInput.second) return 0;
+    fileSize = userInput.first;
+
+    userInput = getUserInput(
+            "Укажите скорость соединения Mbps: ",
+            0.001, 1000000000,
+            "Ошибка: вы указали некорректную скорость соединения. Для завершения программы введите'stop'\n"
+    );
+    if (userInput.second) return 0;
+    speedConnect = userInput.first;
+
+    std::cout << "Скачиваем файл с размером " << fileSize << std::endl;
+    std::cout << "Скорость соединения: " << speedConnect << std::endl;
+
+    return 0;
 }
