d63dff6f1b381662ef97deda32630dad5a59393d
MajotraderLucky <majohead@gmail.com>
2023-12-06 04:41:41 +0300

    feat(main): Finish write the program

--- Module8th/FinalTasksModule8/SteamKiller/main.cpp
+++ Module8th/FinalTasksModule8/SteamKiller/main.cpp
@@ -9,7 +9,7 @@ int main() {
     std::pair<double, bool> userInput;
 
     userInput = getUserInput(
-            "Укажите размер файла для скачивания в байтах: ",
+            "Укажите размер файла для скачивания в мегабайтах: ",
             0.001, 1000000000,
             "Ошибка: вы указали некорректный размер файла. Для завершения программы введите 'stop'\n"
 );
@@ -17,7 +17,7 @@ int main() {
     fileSize = userInput.first;
 
     userInput = getUserInput(
-            "Укажите скорость соединения Mbps: ",
+            "Укажите скорость соединения мегабайт/сек: ",
             0.001, 1000000000,
             "Ошибка: вы указали некорректную скорость соединения. Для завершения программы введите'stop'\n"
     );
@@ -26,6 +26,21 @@ int main() {
 
     std::cout << "Скачиваем файл с размером " << fileSize << std::endl;
     std::cout << "Скорость соединения: " << speedConnect << std::endl;
+
+    double downloaded = 0;
+    int seconds = 0;
+    while (downloaded < fileSize) {
+        downloaded += speedConnect;
+        if (downloaded > fileSize) {
+            downloaded = fileSize;
+        }
+
+        int percentage = static_cast<int>((downloaded / fileSize) * 100);
+        std::cout << "Прошло " << ++seconds << " сек. Скачано " << downloaded << " из " << fileSize << " МБ (" << percentage << "%)." << std::endl;
+    }
+
+    std::cout << "Файл загружен полностью за " << seconds << " секунд." << std::endl;
+
 
     return 0;
 }
