d6a0a06962c7ae9cb5a7719fededf7b39640c595
MajotraderLucky <majohead@gmail.com>
2023-12-27 13:18:30 +0300

    feat(main.cpp): Process of input user data has been create

--- Module8th/FinalTasksModule8/Pendulum/main.cpp
+++ Module8th/FinalTasksModule8/Pendulum/main.cpp
@@ -1,25 +1,41 @@
 #include <iostream>
+#include <stdexcept>
 #include "usersinputcontrol.h"
 
+class StopException : public std::exception {
+public:
+    const char* what() const noexcept override {
+        return "Stop exception occurred";
+    }
+};
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    double initialAmplitude;
+    double finalAmplitude;
 
-    double initialAmplitude, finalAmplitude;
+    try {
+        processUserInput(
+                "Введите начальную амплитуду маятника см: ",
+                0.1, 1000,
+                "Ошибка: вы ввели некорректные данные. Для завершения программы введите 'stop'\n",
+                initialAmplitude
+        );
+        std::cout << "Вы ввели начальную амплитуду маятника: " << initialAmplitude << " cм." << std::endl;
 
-    processUserInput(
-            "Введите начальную амплитуду маятника: ",
-            0.1, 1000,
-            "Ошибка: вы ввели некорректные данные. Для завершения программы введите 'stop'\n",
-            initialAmplitude
-            );
-    std::cout << "Вы ввели начальную амплитуду маятника: " << initialAmplitude << std::endl;
+        processUserInput(
+                "Введите конечную амплитуду маятника: ",
+                0.1, 1000,
+                "Ошибка: вы ввели некорректные данные. Для завершения программы введите'stop'\n",
+                finalAmplitude
+        );
+        std::cout << "Вы ввели конечную амплитуду маятника: " << finalAmplitude << " см." << std::endl;
 
-    processUserInput(
-            "Введите конечную амплитуду маятника: ",
-            0.1, 1000,
-            "Ошибка: вы ввели некорректные данные. Для завершения программы введите'stop'\n",
-            finalAmplitude
-            );
-    std::cout << "Вы ввели конечную амплитуду маятника: " << finalAmplitude << std::endl;
+    } catch (const StopException& e) {
+        std::cerr << e.what() << std::endl;
+        return 0;
+    } catch (const std::runtime_error& e) {
+        std::cerr << "Вы ввели 'stop': " << e.what() << std::endl;
+    }
+
     return 0;
 }
