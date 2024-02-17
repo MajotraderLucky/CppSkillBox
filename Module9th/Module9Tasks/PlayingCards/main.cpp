65399ec072576076a578e36b3e30cd4954531ad0
MajotraderLucky <majohead@gmail.com>
2024-02-18 01:39:49 +0300

    refactor(main): Change the name of two variables

--- Module9th/Module9Tasks/PlayingCards/main.cpp
+++ Module9th/Module9Tasks/PlayingCards/main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 #include "playingcards.h"
 
+int findCardIndex(char card, char charArray[9]) {
+  for (int i = 0; i < 9; i++) {
+    if (charArray[i] == card) {
+      return i;
+    }
+  }
+  return -1;
+}
+
 int main() {
   char firstCard, secondCard;
   bool cardIncorrect;
+  char cardsArray[9] = {'6', '7', '8', '9', '0', 'J', 'Q', 'K', 'A'};
+
   std::cout << "Input the name of the two cards: " << std::endl;
   std::cin >> firstCard >> secondCard;
 
@@ -23,4 +34,10 @@ int main() {
     std::cout << "First card: " << firstCard << "-" << static_cast<int>(firstCard) << std::endl;
     std::cout << "Second card: " << secondCard << "-" << static_cast<int>(secondCard) << std::endl;
   }
+
+  int firstCardIndex = findCardIndex(firstCard, cardsArray);
+  int secondCardIndex = findCardIndex(secondCard, cardsArray);
+
+  std::cout << "First card index: " << firstCardIndex << std::endl;
+  std::cout << "Second card index: " << secondCardIndex << std::endl;
 }
