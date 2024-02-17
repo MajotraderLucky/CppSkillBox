4f19b65795f5e854210e01cf3c24a6d3e184d0a4
MajotraderLucky <majohead@gmail.com>
2024-02-18 02:30:48 +0300

    feat(main): The task has been solved, but verification is required

--- Module9th/Module9Tasks/PlayingCards/main.cpp
+++ Module9th/Module9Tasks/PlayingCards/main.cpp
@@ -40,4 +40,24 @@ int main() {
 
   std::cout << "First card index: " << firstCardIndex << std::endl;
   std::cout << "Second card index: " << secondCardIndex << std::endl;
+
+  bool firstCardA = (firstCardIndex == 8 && secondCardIndex == 0);
+  bool secondCardA = (firstCardIndex == 0 && secondCardIndex == 8);
+  bool firstCardIsBigger = (firstCardIndex > secondCardIndex);
+  bool secondCardIsBigger = (firstCardIndex < secondCardIndex);
+  bool errorCard = (firstCardIndex == -1 || secondCardIndex == -1);
+
+  if (!errorCard) {
+    if (secondCardA) {
+      std::cout << "The first card is bigger" << std::endl;
+    } else if (firstCardA) {
+      std::cout << "The second card is bigger" << std::endl;
+    } else if (firstCardIsBigger) {
+      std::cout << "The first card is bigger" << std::endl;
+    } else if (secondCardIsBigger) {
+      std::cout << "The second card is bigger" << std::endl;
+    }
+  } else {
+    std::cout << "Error: one of the cards is incorrect" << std::endl;
+  }
 }
