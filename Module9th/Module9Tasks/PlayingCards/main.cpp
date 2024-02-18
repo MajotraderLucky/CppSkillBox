5a78fccbbb40ae4738c102e7c7a98101882eb955
MajotraderLucky <majohead@gmail.com>
2024-02-18 03:23:44 +0300

    refactor(main): Delete unnecessary checks

--- Module9th/Module9Tasks/PlayingCards/main.cpp
+++ Module9th/Module9Tasks/PlayingCards/main.cpp
@@ -18,23 +18,6 @@ int main() {
   std::cout << "Input the name of the two cards: " << std::endl;
   std::cin >> firstCard >> secondCard;
 
-  if (!isValidCard(firstCard)) {
-    std::cout << "The data of the first card is incorrect!" << std::endl;
-  } else {
-    std::cout << "The data of the first card is correct" << std::endl;
-  }
-  
-  if (!isValidCard(secondCard)) {
-    std::cout << "The data of the second card is incorrect!" << std::endl;
-  } else {
-    std::cout << "The data of the second card is correct" << std::endl;
-  }
-
-  if (isValidCard(firstCard) && isValidCard(secondCard)) {
-    std::cout << "First card: " << firstCard << "-" << static_cast<int>(firstCard) << std::endl;
-    std::cout << "Second card: " << secondCard << "-" << static_cast<int>(secondCard) << std::endl;
-  }
-
   int firstCardIndex = findCardIndex(firstCard, cardsArray);
   int secondCardIndex = findCardIndex(secondCard, cardsArray);
 
