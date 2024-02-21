57cae5e462bbbb8c0045474ad44d788a7a5003ad
MajotraderLucky <majohead@gmail.com>
2024-02-21 03:50:08 +0300

    feat(main): Add check input the letter point

--- Module9th/Module9Tasks/ChessMoves/main.cpp
+++ Module9th/Module9Tasks/ChessMoves/main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+  std::cout << "Enter a start point on the chess board" << std::endl;
+  char startPoint;
+  std::cin >> startPoint;
+
+  char letter_coordinat[8] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
+  int count = 0;
+  for (int i = 0; i < 8; i++) {
+    if (letter_coordinat[i] != startPoint) {
+      count++;
+    }
+  }
+
+  if (count == 8) {
+    std::cout << "The point is not on the board" << std::endl;
+  } else {
+    std::cout << "The point is on the board" << std::endl;
+  }
+
   return 0;
 }
