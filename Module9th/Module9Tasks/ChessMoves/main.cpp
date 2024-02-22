7bf8ce4defce6699e3d3030111081b454ce731bc
MajotraderLucky <majohead@gmail.com>
2024-02-22 16:07:55 +0300

    feat(main): Add process and check of the start and end points of the figure`s move

--- Module9th/Module9Tasks/ChessMoves/main.cpp
+++ Module9th/Module9Tasks/ChessMoves/main.cpp
@@ -2,21 +2,40 @@
 
 int main() {
   std::cout << "Enter a start point on the chess board" << std::endl;
-  char startPoint;
-  std::cin >> startPoint;
+  char letterStartPoint;
+  int numberStartPoint;
+  std::cin >> letterStartPoint >> numberStartPoint;
 
   char letter_coordinat[8] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
   int count = 0;
   for (int i = 0; i < 8; i++) {
-    if (letter_coordinat[i] != startPoint) {
+    if (letter_coordinat[i] != letterStartPoint) {
       count++;
     }
   }
 
-  if (count == 8) {
-    std::cout << "The point is not on the board" << std::endl;
+  if ((count == 8) || (numberStartPoint < 1 || numberStartPoint > 8)) {
+    std::cout << "The start point is not on the board" << std::endl;
   } else {
-    std::cout << "The point is on the board" << std::endl;
+    std::cout << "The start point is on the board" << std::endl;
+  }
+
+  std::cout << "Enter an end point on the chess board" << std::endl;
+  char letterEndPoint;
+  int numberEndPoint;
+  std::cin >> letterEndPoint >> numberEndPoint;
+
+  count = 0;
+  for (int i = 0; i < 8; i++) {
+    if (letter_coordinat[i] != letterEndPoint) {
+      count++;
+    }
+  }
+
+  if ((count == 8) || (numberEndPoint < 1 || numberEndPoint > 8 || (numberEndPoint == numberStartPoint && letterEndPoint == letterStartPoint))) {
+    std::cout << "The end point is not on the board" << std::endl;
+  } else {
+    std::cout << "The end point is on the board" << std::endl;
   }
 
   return 0;
