3d478368fe071b327780f9821f699901802d37b8
MajotraderLucky <majohead@gmail.com>
2024-02-24 20:55:21 +0300

    feat(main): Create the isKingMove function with which determine whether the move corresponds to the move of the chess king

--- Module9th/Module9Tasks/ChessMoves/main.cpp
+++ Module9th/Module9Tasks/ChessMoves/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
-#include <cmath>
 #include "chessmoves.h"
+#include <cmath>
+
+bool isKingMove(char letterStartPoint, int numberStartPoint, char letterEndPoint, int numberEndPoint) {
+    // Converting char to ASCII code
+    int startLetter = static_cast<int>(letterStartPoint);
+    int endLetter = static_cast<int>(letterEndPoint);
+
+    // Checking if the move is valid for a king
+    if(abs(startLetter - endLetter) <= 1 && abs(numberStartPoint - numberEndPoint) <= 1) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 int main() {
   std::cout << "Enter a start point on the chess board" << std::endl;
@@ -31,11 +44,14 @@ int main() {
   }
 
   bool isKnightMoveValid = isKnightMove(letterStartPoint, numberStartPoint, letterEndPoint, numberEndPoint);
+  bool isKingMoveValid = isKingMove(letterStartPoint, numberStartPoint, letterEndPoint, numberEndPoint);
 
   if (isStartPointValid && isEndPointValid) {
     std::cout << "The move of the figure is valid" << std::endl;
     std::cout << std::boolalpha;  // Print boolean as true/false instead of 1/0.
-    std::cout << isKnightMoveValid << std::endl;
+    std::cout << "isKnightMoveValid: " << isKnightMoveValid << std::endl;
+    std::cout << "isKingMoveValid: " << isKingMoveValid << std::endl;
+
   } else {
     std::cout << "The move of the figure is not valid" << std::endl;
   }
